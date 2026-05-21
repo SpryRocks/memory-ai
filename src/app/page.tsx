24e3c30c343d6c914ebfb55e62e53a08e3409cb2
Maksym Zhemerenko <maxim.zhemerenko@icloud.com>
2026-05-21 22:42:26 +0200

    add structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,5 @@
 "use client";
 
-import {FC} from 'react';
-import {ChatBox, ChatBoxProps, ChatConversationHeader} from "@mui/x-chat";
+import {Main} from '@/features';
 
-const createEmptyStream = () => new ReadableStream({
-  start(controller) {
-    controller.close();
-  },
-});
-
-const adapter: ChatBoxProps["adapter"] = {
-  sendMessage: async () => {
-    return createEmptyStream();
-  }
-}
-
-const ConversationHeader: FC = (props) =>
-  <ChatConversationHeader {...props}>
-    Memory AI
-  </ChatConversationHeader>
-
-const Home: FC = () =>
-  <ChatBox
-    adapter={adapter}
-    slots={{
-      conversationHeader: ConversationHeader,
-    }}
-    sx={{
-      '& .MuiChatMessage-avatar': {
-        visibility: 'hidden',
-      },
-    }}
-  />;
-
-export default Home;
+export default Main;
